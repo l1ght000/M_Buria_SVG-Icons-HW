a591ad3d8dfe6c343cd4428d7e52bbe9f10fdadf
Nikita-Buria <m_buria@fanshaweonline.ca>
2023-01-21 20:57:42 -0500

    finish and solve some js issues

--- js/main.js
+++ js/main.js
@@ -1,9 +1,107 @@
 console.log('JavaScript is running!');
 
-let Icon = document.querySelector(".Icon")
+let bitmapgraphic = document.querySelector("#bitmapgraphic")
+
+bitmapgraphic.addEventListener('click', logID);
 
 function logID() {
     console.log(this.id);
 }
 
-Icon.addEventListener('click', logID);
+
+
+
+let bitmapgraphic2 = document.querySelector("#bitmapgraphic2")
+
+bitmapgraphic2.addEventListener('click', logID);
+
+function logID() {
+    console.log(this.id);
+}
+
+
+
+let bitmapgraphic3 = document.querySelector("#bitmapgraphic3")
+
+bitmapgraphic3.addEventListener('click', logID);
+
+function logID() {
+    console.log(this.id);
+}
+
+
+
+let bitmapgraphic4 = document.querySelector("#bitmapgraphic4")
+
+bitmapgraphic4.addEventListener('click', logID);
+
+function logID() {
+    console.log(this.id);
+}
+
+
+
+
+let bitmapgraphic5 = document.querySelector("#bitmapgraphic5")
+
+bitmapgraphic5.addEventListener('click', logID);
+
+function logID() {
+    console.log(this.id);
+}
+
+
+
+
+let bitmapgraphic6 = document.querySelector("#bitmapgraphic6")
+
+bitmapgraphic6.addEventListener('click', logID);
+
+function logID() {
+    console.log(this.id);
+}
+
+
+
+
+let bitmapgraphic7 = document.querySelector("#bitmapgraphic7")
+
+bitmapgraphic7.addEventListener('click', logID);
+
+function logID() {
+    console.log(this.id);
+}
+
+
+
+
+let bitmapgraphic8 = document.querySelector("#bitmapgraphic8")
+
+bitmapgraphic8.addEventListener('click', logID);
+
+function logID() {
+    console.log(this.id);
+}
+
+
+
+
+let bitmapgraphic9 = document.querySelector("#bitmapgraphic9")
+
+bitmapgraphic9.addEventListener('click', logID);
+
+function logID() {
+    console.log(this.id);
+}
+
+
+
+
+let bitmapgraphic10 = document.querySelector("#bitmapgraphic10")
+
+bitmapgraphic10.addEventListener('click', logID);
+
+function logID() {
+    console.log(this.id);
+}
+
